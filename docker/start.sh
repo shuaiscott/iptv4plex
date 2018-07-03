8a35924d351e860c55c6026443bdf905719cc513
Scott Carlson <shuaiscott@users.noreply.github.com>
2018-07-03 01:27:03 -0500

    Making Port an int instead of a string

--- docker/start.sh
+++ docker/start.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 if [ ! -f /app/proxysettings.json ]; then
-  echo "{\"port\": \"$port\", \"m3u8url\": \"$m3u8urllist\", \"tunerlimits\": \"$tunerlimitslist\", \"xmlurl\": \"$xmlurllist\", \"ip\": \"$ipaddress\"}" > /app/proxysettings.json
+  echo "{\"port\": $port, \"m3u8url\": \"$m3u8urllist\", \"tunerlimits\": \"$tunerlimitslist\", \"xmlurl\": \"$xmlurllist\", \"ip\": \"$ipaddress\"}" > /app/proxysettings.json
 fi
 
 python /app/iptv4plex.py -hl
-
-
